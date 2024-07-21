b773368dc1cf9868352ca754a476a96734d45c21
shebin <mrshebinks@gmail.com>
2024-07-21 18:34:16 +0530

    added device list to account details

--- frontend/lib/Screens/account_details_screen/account_details_screen.dart
+++ frontend/lib/Screens/account_details_screen/account_details_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:saron/api/device_model/device.dart';
 
 class AccountDetailsScreen extends StatelessWidget {
   final String name;
   final String email;
+  final List<Device> devices;
 
   const AccountDetailsScreen({
     super.key,
     required this.name,
     required this.email,
+    required this.devices,
   });
 
   @override
@@ -26,18 +29,58 @@ class AccountDetailsScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 16.0),
+            const Text(
+              'Name',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 8.0),
             AccountDetailsTile(
               icon: Icons.person,
               label: 'Name',
-              value: name,
+              value: name.toUpperCase(),
             ),
             const SizedBox(height: 16.0),
+            const Text(
+              'Email',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 8.0),
             AccountDetailsTile(
               icon: Icons.email,
               label: 'Email',
               value: email,
             ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Devices',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
             const SizedBox(height: 8.0),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Color.fromARGB(255, 81, 53, 53),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: devices.map((device) {
+                  return DeviceTile(device: device);
+                }).toList(),
+              ),
+            ),
           ],
         ),
       ),
@@ -62,7 +105,7 @@ class AccountDetailsTile extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8.0),
-        color: Color.fromARGB(255, 96, 66, 66),
+        color: Color.fromARGB(255, 81, 53, 53),
       ),
       padding: const EdgeInsets.all(16.0),
       child: Row(
@@ -77,13 +120,69 @@ class AccountDetailsTile extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  label,
+                  value,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DeviceTile extends StatelessWidget {
+  final Device device;
+
+  const DeviceTile({
+    Key? key,
+    required this.device,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        color: Color.fromARGB(255, 87, 58, 58),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 8.0,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Icon(
+            Icons.devices,
+            color: Colors.white,
+          ),
+          const SizedBox(width: 16.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  device.deviceName!.toUpperCase(),
                   style: const TextStyle(
                     fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
                 const SizedBox(height: 4.0),
-                Text(value),
+                Text(
+                  'Device ID: ${device.deviceId ?? 'N/A'}',
+                  style: const TextStyle(color: Colors.white),
+                ),
               ],
             ),
           ),
